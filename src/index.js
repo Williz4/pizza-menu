ab0ad1886367430770029af989e09361c7a581d9
Wilson Mwangeka <wilsonmwangeka@gmail.com>
2023-08-22 20:48:15 +0300

    Updated to setting classes and text conditionally

--- src/index.js
+++ src/index.js
@@ -80,14 +80,15 @@ function Menu() {
     )
 }
 
-function Pizza(props) {
+function Pizza({ pizzaObj }) {
+  //if(pizzaObj.soldOut) return null;
   return (
-      <li className='pizza'>
-      <img src={props.pizzaObj.photoName} alt={props.name}/>
+      <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
+      <img src={pizzaObj.photoName} alt={pizzaObj.name}/>
       <div>
-      <h3>{props.pizzaObj.name}</h3>
-      <p>{props.pizzaObj.ingredients}</p>
-      <span>{props.pizzaObj.price}</span>
+      <h3>{pizzaObj.name}</h3>
+      <p>{pizzaObj.ingredients}</p>
+      <span>{pizzaObj.price}</span>
       </div>
     </li>
   )
